c94acee3d9b693aa82498ed4a3143930d1fd603b
Guilherme Xavier <xguilherme1@gmail.com>
2024-08-15 17:27:23 -0300

    exibe result

--- src/components/AppComponents/content/QrCodeContent/index.tsx
+++ src/components/AppComponents/content/QrCodeContent/index.tsx
@@ -9,7 +9,7 @@ const QrCodeContent: React.FC<QrCodeContentProps> = ({ hidden }) => {
   const [selected, setSelected] = useState("environment");
   const [startScan, setStartScan] = useState(false);
   const [loadingScan, setLoadingScan] = useState(false);
-  const [data, setData] = useState("");
+  const [data, setData] = useState<any>(null);
 
   const handleError = (err: any) => {
     console.error(err);
@@ -31,17 +31,12 @@ const QrCodeContent: React.FC<QrCodeContentProps> = ({ hidden }) => {
   return (
     <div className="App" hidden={hidden}>
 
+      <p>{data ? data : ''}</p>
       <QrReader
         constraints={{facingMode: "environment"}}
         scanDelay={1000}
         onResult={(result, error) => {
-          if (!!result) {
-            console.log(result);
-          }
-
-          if (!!error) {
-            console.info(error);
-          }
+          if(result) setData(result)
         }}
         
       />
