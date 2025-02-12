17ba753f90d303a965d002a5d4529bfdbea9aa54
Guilherme Xavier <xguilherme1@gmail.com>
2025-02-12 17:20:14 -0300

    pedindo autorização pra acessar a camera

--- src/screens/AppScreens/screenCommonUser/QrCodeContent/index.tsx
+++ src/screens/AppScreens/screenCommonUser/QrCodeContent/index.tsx
@@ -1,40 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { QrReader } from "react-qr-reader";
-
 
 interface QrCodeContentProps {
   hidden: boolean;
 }
 
 const QrCodeContent: React.FC<QrCodeContentProps> = ({ hidden }) => {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
 
+  useEffect(() => {
+    // Solicita permissão para acessar a câmera
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(() => setHasPermission(true))
+      .catch(() => setHasPermission(false));
+  }, []);
 
   const handleScanResult = (result: any, error: any) => {
-    if(result){
-      console.log(result, 'LEITURA CONCLUIDA');
-    } else {
-
+    if (result) {
+      console.log(result, "LEITURA CONCLUÍDA");
     }
-  }
+  };
 
   return (
     <div className="h-full" hidden={hidden}>
-        <div >
-          <h1 className='text-black text-3xl font-bold text-center mb-2'>QR Code</h1>
-          <p className='text-black text-2xl font-light text-center mb-4'>Faça a leitura do QR Code</p>
-        </div>
-        <div>
+      <div>
+        <h1 className="text-black text-3xl font-bold text-center mb-2">
+          QR Code
+        </h1>
+        <p className="text-black text-2xl font-light text-center mb-4">
+          Faça a leitura do QR Code
+        </p>
+      </div>
+
+      <div>
+        {hasPermission === null ? (
+          <p className="text-center text-black">Verificando permissão...</p>
+        ) : hasPermission === false ? (
+          <p className="text-center text-red-500">
+            Permissão negada. Ative a câmera manualmente.
+          </p>
+        ) : (
           <QrReader
-            constraints={{facingMode: "environment"}}
+            constraints={{ facingMode: "environment" }}
             scanDelay={2000}
             onResult={handleScanResult}
-            className='mt-auto'
+            className="mt-auto"
           />
-        </div>
-
-
-
-      
+        )}
+      </div>
     </div>
   );
 };
