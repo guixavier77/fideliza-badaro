8733db32086650c15d4588bdfd7485680b0006ab
Guilherme Xavier <xguilherme1@gmail.com>
2025-02-12 19:37:38 -0300

    build

--- src/screens/AppScreens/screenCommonUser/QrCodeContent/index.tsx
+++ src/screens/AppScreens/screenCommonUser/QrCodeContent/index.tsx
@@ -85,30 +85,7 @@ const QrCodeContent: React.FC<QrCodeContentProps> = ({ hidden }) => {
         </div>
       ) : (
         <Scanner
-          formats={[
-            "qr_code",
-            "micro_qr_code",
-            "rm_qr_code",
-            "maxi_code",
-            "pdf417",
-            "aztec",
-            "data_matrix",
-            "matrix_codes",
-            "dx_film_edge",
-            "databar",
-            "databar_expanded",
-            "codabar",
-            "code_39",
-            "code_93",
-            "code_128",
-            "ean_8",
-            "ean_13",
-            "itf",
-            "linear_codes",
-            "upc_a",
-            "upc_e",
-          ]}
-        
+          
           onScan={(detectedCodes) => {
             onScannerResult(detectedCodes[0].rawValue);
           }}
@@ -123,7 +100,7 @@ const QrCodeContent: React.FC<QrCodeContentProps> = ({ hidden }) => {
             zoom: true,
             finder: true,
           }}
-          allowMultiple={true}
+          // allowMultiple={true}
           scanDelay={2000}
         />
         )}
