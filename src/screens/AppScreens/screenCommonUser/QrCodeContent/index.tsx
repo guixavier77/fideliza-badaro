29bebda437e305d5fcf84c3d74ad702aea2d5967
Guilherme Xavier <xguilherme1@gmail.com>
2025-02-12 19:21:45 -0300

    build

--- src/screens/AppScreens/screenCommonUser/QrCodeContent/index.tsx
+++ src/screens/AppScreens/screenCommonUser/QrCodeContent/index.tsx
@@ -84,8 +84,49 @@ const QrCodeContent: React.FC<QrCodeContentProps> = ({ hidden }) => {
           </button>
         </div>
       ) : (
-        <Scanner onScan={onScannerResult} />
-      )}
+        <Scanner
+          formats={[
+            "qr_code",
+            "micro_qr_code",
+            "rm_qr_code",
+            "maxi_code",
+            "pdf417",
+            "aztec",
+            "data_matrix",
+            "matrix_codes",
+            "dx_film_edge",
+            "databar",
+            "databar_expanded",
+            "codabar",
+            "code_39",
+            "code_93",
+            "code_128",
+            "ean_8",
+            "ean_13",
+            "itf",
+            "linear_codes",
+            "upc_a",
+            "upc_e",
+          ]}
+        
+          onScan={(detectedCodes) => {
+            console.log(detectedCodes[0].rawValue);
+          }}
+          onError={(error) => {
+            console.log(`onError: ${error}'`);
+          }}
+          styles={{ container: { height: "400px", width: "350px" } }}
+          components={{
+            audio: true,
+            onOff: true,
+            torch: true,
+            zoom: true,
+            finder: true,
+          }}
+          allowMultiple={true}
+          scanDelay={2000}
+        />
+        )}
     </div>
   );
 };
